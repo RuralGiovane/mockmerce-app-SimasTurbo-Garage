54bea742347fc4eed427df8ba39a08cd765d0bfd
RuralGiovane <giovaneamato@gmail.com>
2026-08-20 10:07:33 -0300

    Docs: removing comments

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -4,12 +4,12 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 30, // 30s
+      staleTime: 1000 * 30,
 
-      gcTime: 1000 * 60 * 5, // 5min,
+      gcTime: 1000 * 60 * 5,
 
       retry: (failureCount, error) => {
-        if (error instanceof ApiError && error?.status >= 400 && error?.status < 500) {
+        if (error instanceof ApiError && error.status >= 400 && error.status < 500) {
           return false;
         }
 
